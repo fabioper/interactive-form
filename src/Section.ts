676b76891704ac71f15e2aa75a490c173e6b38b3
Fábio Pereira <fabiopereira.fps@gmail.com>
2020-03-03 10:57:33 -0300

    Add inputmask

--- src/Section.ts
+++ src/Section.ts
@@ -196,7 +196,6 @@ export default class Section {
     }
 
     private getUserInfoListingMarkup(): string {
-        console.log(this.state)
         return `
                 <div>
                     <h3>Informações de Contato</h3>
